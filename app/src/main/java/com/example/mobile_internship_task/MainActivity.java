8127174fbe065d9a6361acf8b813dca146427fb7
DonatasAbro <donatas.abro@gmail.com>
2022-07-29 20:35:20 +0300

    renamed several variables and functions

--- app/src/main/java/com/example/mobile_internship_task/MainActivity.java
+++ app/src/main/java/com/example/mobile_internship_task/MainActivity.java
@@ -20,25 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String jsonPath = "json/dog_urls.json";
-        String dogUrlsStr = file2String(jsonPath);
-        ArrayList<String> dogUrlsArray = new ArrayList<>();
+        String dogUrlsStr = readFileContent(jsonPath);
+        ArrayList<String> dogUrls = new ArrayList<>();
         ListView dogUrlsList = findViewById(R.id.dogUrlsList);
         
         try {
-            JSONObject jsonObject = new JSONObject(dogUrlsStr);
-            JSONArray jsonArray = jsonObject.getJSONArray("urls");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                dogUrlsArray.add(jsonArray.get(i).toString());
+            JSONObject dogUrlsJsonObject = new JSONObject(dogUrlsStr);
+            JSONArray dogUrlsJsonArray = dogUrlsJsonObject.getJSONArray("urls");
+            for (int i = 0; i < dogUrlsJsonArray.length(); i++) {
+                dogUrls.add(dogUrlsJsonArray.get(i).toString());
             }
         } catch (JSONException exception) {
             Log.e("JsonParser error", "unexpected JSON exception", exception);
         }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, dogUrlsArray);
-        dogUrlsList.setAdapter(arrayAdapter);
+        ArrayAdapter<String> dogUrlsArrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, dogUrls);
+        dogUrlsList.setAdapter(dogUrlsArrayAdapter);
     }
 
-    private String file2String(String filepath){
+    private String readFileContent(String filepath){
         String returnable = "";
         try {
             InputStream inputStream = getAssets().open(filepath);
